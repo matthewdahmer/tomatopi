e1ac5b1b972f3cc137f55fa96a0e5c41c3536e5d
matthewdahmer <matthew.dahmer@gmail.com>
2014-06-28 14:49:07 +0000

    Make intermediate changes

--- relay.py
+++ relay.py
@@ -2,9 +2,15 @@ import serial
 import time
 import email_ip
 
+fid = open("log.txt", "w")
+
 email_ip.email_ip_addr()
 
+tnow = time.localtime()
+
 ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+
 
 # Startup Test Pattern
 ser.write('0')
@@ -15,14 +21,17 @@ ser.write('0')
 time.sleep(5)
 ser.write('1')
 
-while 1 > 0:
+
+
+while tnow.tm_yday < 260:
         tnow = time.localtime()
-	if tnow.tm_hour==13 and tnow.tm_min==55:
-	    print("It's Time!")
+        timestring = "{}.{}.{}.{}.{}.{}".format(tnow.tm_year, tnow.tm_mon, tnow.tm_mday, tnow.tm_hour, tnow.tm_min, tnow.tm_sec)
+	if tnow.tm_hour==6 and tnow.tm_min==30:
+	    print("{}: Watering".format(timestring)
 	    ser.write('0')
 	    time.sleep(60)
 	    ser.write('1')
 	else:
 	    time.sleep(60)
-	    print("Not Time")
+	    print("{}: Not watering".format(timestring))
 	    ser.write('1')
---
+++ waternow.py
@@ -0,0 +1,24 @@
+import serial
+import time
+import email_ip
+
+fid = open("log.txt", "w")
+
+email_ip.email_ip_addr()
+
+tnow = time.localtime()
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+timestring = "{}.{}.{}.{}.{}.{}".format(tnow.tm_year, tnow.tm_mon, tnow.tm_mday, tnow.tm_hour, tnow.tm_min, tnow.tm_sec)
+print("{}: Water On".format(timestring))
+fid.write("{}: Water On\n".format(timestring))
+
+ser.write('0')
+time.sleep(600)
+ser.write('1')
+
+print("{}: Water Off\n".format(timestring))
+
+fid.write("{}: Water Off\n".format(timestring))
+fid.close()
